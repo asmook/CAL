5fa9414cfc135b5e58262e26589a1782a72ff2fb
Lauren Tejada <lauren.tejada25@gmail.com>
2018-04-25 19:41:37 -0600

    navbar updated

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { Route, Link } from 'react-router-dom'
-import logo from '../logo.svg';
-import '../App.css';
+// import '../App.css';
 import axios from 'axios'
+import "./navbar.css";
 
 class Navbar extends Component {
     constructor() {
@@ -34,9 +34,9 @@ class Navbar extends Component {
         
         return (
             <div>
-
                 <header className="navbar App-header" id="nav-container">
-                    <div className="col-4" >
+                        <div id="top-filler"></div>
+                            <h1 className="App-title">StoryBook</h1>
                         {loggedIn ? (
                             <section className="navbar-section">
                                 <Link to="#" className="btn btn-link text-secondary" onClick={this.logout}>
@@ -44,24 +44,18 @@ class Navbar extends Component {
 
                             </section>
                         ) : (
-                                <section className="navbar-section">
-                                    <Link to="/" className="btn btn-link text-secondary">
-                                        <span className="text-secondary">home</span>
-                                        </Link>
-                                    <Link to="/login" className="btn btn-link text-secondary">
+                            <section className="navbar-section">
+                                <Link to="/" className="btn btn-link text-secondary">
+                                    <span className="text-secondary">home</span>
+                                </Link>
+                                <Link to="/login" className="btn btn-link text-secondary">
                                     <span className="text-secondary">login</span>
-				</Link>
-                                    <Link to="/signup" className="btn btn-link">
+				                </Link>
+                                <Link to="/signup" className="btn btn-link">
                                     <span className="text-secondary">sign up</span>
-				</Link>
-                                </section>
-                            )}
-                    </div>
-                    <div className="col-4 col-mr-auto">
-                    <div id="top-filler"></div>
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h1 className="App-title">StoryBook</h1>
-                    </div>
+				                </Link>
+                            </section>
+                        )}
                 </header>
             </div>
 
