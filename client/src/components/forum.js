4eeec054eed9fc400fd450890b33a00dda5d62b2
Audra Mook <asmook87@hotamil.com>
2018-04-27 17:36:01 -0600

    forum page

--- client/src/components/forum.js
+++ client/src/components/forum.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Forum = () => (
-<div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
+    <Fragment>
+        <div class="genres">
+            <ul>
+                <li><a href="#">funny</a></li>
+                <li><a href="#">spooky</a></li>
+                <li><a href="#">sexy</a></li>
+                <li><a href="#">bougie</a></li>
+                <li><a href="#">dad jokes</a></li>
+                <li><a href="#">nsfw</a></li>
+            </ul>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="createNew float-right">
+					 <button type="button" className="btn btn-outline-info float-right">Create New Story</button>
+                </div>
+                <div className = "posts">
+                </div>
+            </div>
+        </div>
+    </Fragment>
 )
 
 export default Forum;
