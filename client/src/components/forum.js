9cffb97801e915634726c58f8d28b5b9717df8b5
Audra Mook <asmook87@hotamil.com>
2018-04-27 18:10:47 -0600

    friday evening

--- client/src/components/forum.js
+++ client/src/components/forum.js
@@ -17,7 +17,75 @@ const Forum = () => (
                 <div className="createNew float-right">
 					 <button type="button" className="btn btn-outline-info float-right">Create New Story</button>
                 </div>
-                <div className = "posts">
+                <div className="posts">
+                    <ul>
+                        <li className="card border-info mb-3">
+                            <div class="card-header">Cinderella Rollerskating</div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                   sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </li>
+                         <li className="card border-info mb-3">
+                            <div class="card-header">Black mirror and the Truman show</div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                   sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </li>
+                         <li className="card border-info mb-3">
+                            <div class="card-header">All you need is farts n skateboards</div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                   sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </li>
+                         <li className="card border-info mb-3">
+                            <div class="card-header">Ariel is slutty</div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                   sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </li>
+                         <li className="card border-info mb-3">
+                            <div class="card-header">Sarah is a crumdgeon</div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                   sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </li>
+                         <li className="card border-info mb-3">
+                            <div class="card-header">Chris is stinky</div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                   sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
